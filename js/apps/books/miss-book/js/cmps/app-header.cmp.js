6d45cddb50efd045ce805ef31aeb1bb38f13ef11
Dror Uzi <dror.uzi5@gmail.com>
2019-11-10 22:05:51 +0200

    a

--- js/apps/books/miss-book/js/cmps/app-header.cmp.js
+++ js/apps/books/miss-book/js/cmps/app-header.cmp.js
@@ -5,9 +5,9 @@ import theRouter from '../routes.js'
 export default {
     router: theRouter,
     template: `
-        <div class="home-main-header">
+        <div class="book-home-main-header">
             <h1>MISS BOOK</h1>
-            <nav class="nav-bar"> 
+            <nav class="book-nav-bar"> 
                 <router-link to="/" exact>Home Page</router-link> 
                 <router-link to="/book">Books</router-link>
                 <router-link class="add-link" to="/add">Add A Book</router-link>
